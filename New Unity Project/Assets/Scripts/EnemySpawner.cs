f6a21888f4f9e6df5fc0458b141c0cbd85275b60
ComradeHappyPug <esther.strathy@digipen.edu>
2020-02-05 09:43:07 -0800

    created cloud scroll script, destroy character on collision with cactus

--- New Unity Project/Assets/Scripts/EnemySpawner.cs
+++ New Unity Project/Assets/Scripts/EnemySpawner.cs
@@ -1,4 +1,10 @@
-﻿using System.Collections;
+﻿/*
+ * EnemySpawner.cs
+ * By: Isaiah
+ * Description: randomly spawns in cacti 
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
