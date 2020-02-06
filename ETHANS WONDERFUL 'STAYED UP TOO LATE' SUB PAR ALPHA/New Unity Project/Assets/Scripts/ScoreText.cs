73ece25079445178eb0b24d1cd6ed0082bbc2382
Younget <48969941+Younget@users.noreply.github.com>
2020-02-06 07:46:37 -0800

    Ethan's alpha

---
+++ ETHANS WONDERFUL 'STAYED UP TOO LATE' SUB PAR ALPHA/New Unity Project/Assets/Scripts/ScoreText.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreText : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Text highScore;
+
+    public void Score()
+    {
+        Text myText = GetComponent<Text>();
+        myText.text = FindObjectOfType<Score>().playerScore.ToString();
+
+
+    }
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Score();
+    }
+}
---
+++ ETHANS WONDERFUL 'STAYED UP TOO LATE' SUB PAR ALPHA/New Unity Project/Assets/Scripts/StartGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void StartTheGame()
+    {
+
+        SceneManager.LoadScene("MainLevel");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
